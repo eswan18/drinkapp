f1a9a0e51f7a2e8b135d66299790e08684333f99
Ethan Swan <eswan18@rocketmail.com>
2021-07-05 21:02:09 -0500

    Ignore vim swapfiles.

--- backend/drinkapi/main.py
+++ backend/drinkapi/main.py
@@ -3,7 +3,7 @@ import datetime as dt
 from uuid import uuid4
 from collections import defaultdict
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 from .user import User
 from .drink import Drink
@@ -20,9 +20,10 @@ used_usernames = set()
 def read_user(user_id: str):
     if user_id in users:
         user = users[user_id]
-        return {'user_id': user_id, 'username': user.username}
+        return user
     else:
-        return 404
+        if user_id not in users:
+            raise HTTPException(status_code=404, detail='User not found')
 
 
 @app.post('/users/')
@@ -30,18 +31,26 @@ def create_user(user: User):
     user_dict = user.dict()
     username = user.username
     if username in used_usernames:
-        return 401
+        raise HTTPException(status_code=409, detail='Requested username already exists')
     user_id = str(uuid4())
     users[user_id] = user
     used_usernames.add(username)
     return user_dict, user_id
 
 
+@app.get('/drinks/{user_id}')
+def read_drinks(user_id: str):
+    if user_id not in users:
+        raise HTTPException(status_code=404, detail='User not found')
+    bac = sum([drink.current_bac for drink in drinks])
+    return {'bac': bac}
+
+
 @app.post('/drinks/')
 def create_drink(user_id: str, drink_grams: int):
     if user_id not in users:
-        return 404
-    user = Users[user_id].dict()
+        raise HTTPException(status_code=404, detail='User not found')
+    user = users[user_id].dict()
     user_wt_grams = user.wt_grams
     user_sex = user.sex
     drink = Drink(
@@ -53,7 +62,4 @@ def create_drink(user_id: str, drink_grams: int):
     drinks[user_id].append(drink)
 
 
-@app.get('/drinks/{user_id}')
-def read_drinks(user_id: str):
-    bac = sum([drink.current_bac for drink in drinks])
-    return {'bac': bac}
+
