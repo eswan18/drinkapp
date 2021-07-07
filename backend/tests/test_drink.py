581e6328b55057f9dec5d8aeda676d90f16f67e2
Ethan Swan <eswan18@rocketmail.com>
2021-07-06 19:25:16 -0500

    Clean up drink tests.

--- backend/tests/test_drink.py
+++ backend/tests/test_drink.py
@@ -6,6 +6,19 @@ import pytest
 from drinkapi.drink import Drink
 from drinkapi.user import UserSex
 
+drinks_and_final_bacs = [
+    (
+        Drink(
+            drink_grams=56,
+            user_wt_grams=54480,
+            user_sex=UserSex.female,
+            time=dt.datetime.now() - dt.timedelta(hours=5)
+        ),
+        0.11189,
+    )
+]
+
+
 def near_equal(float_a, float_b):
     '''
     Compare floats for near-equality.
@@ -13,17 +26,11 @@ def near_equal(float_a, float_b):
     X = 0.0001
     return (float_a - X) < float_b < (float_a + X)
 
-def test_hours_since():
-    an_hour_ago = dt.datetime.now() - dt.timedelta(hours=1)
-    drink = Drink(3, 3943, UserSex.male, time=an_hour_ago)
-    assert 0.99 <= drink.hours_since <= 1.01
+@pytest.mark.parametrize('drink', [drink for (drink, bac) in drinks_and_final_bacs])
+def test_hours_since(drink):
+    hours_since = (dt.datetime.now() - drink.time) / dt.timedelta(hours=1)
+    assert near_equal(drink.hours_since, hours_since)
 
-@pytest.mark.parametrize(
-    'args,hrs,expected',
-    [
-        pytest.param((56, 54480, UserSex.female, dt.datetime.now()), 5, 0.11189)
-    ],
-)
-def test_decayed_bac(args: Dict, hrs: float, expected: float):
-    drink = Drink(*args)
-    assert near_equal(drink.decayed_bac(hrs), expected)
+@pytest.mark.parametrize('drink,final_bac', drinks_and_final_bacs)
+def test_decayed_bac(drink, final_bac):
+    assert near_equal(drink.current_bac, final_bac)
